c4ca5980db011d3381bb645a6b8ec620273e8da5
TehKittyCat <tehkittycat@weirdgloop.org>
2023-04-21 18:52:16 -0400

    Fix invalid ALTER TABLE in patch-gug_group-field.sql.
    Change-Id: I5ecbf92f49a9f8958edd46a3742dc42d829fcfbf

--- db_patches/patch-gug_group-field.sql
+++ db_patches/patch-gug_group-field.sql
@@ -1,2 +1,2 @@
 -- Changes the length of groups from 14 to 255 similar to user_group
-ALTER TABLE /*_*/global_user_groups MODIFY gug_group varbinary(255) NOT NULL NULL default '';
+ALTER TABLE /*_*/global_user_groups MODIFY gug_group varbinary(255) NOT NULL default '';
