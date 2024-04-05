b87e33f312aab6038126cac1772ca7b5640f9af7
xtex <xtexchooser@duck.com>
2024-04-05 23:14:58 +0800

    Replace UserGroupMembership::getGroupMemberName with Language::getGroupMemberName
    Change-Id: I7b9f43406bdbe7187db020e794baca406e9bc618
    (cherry picked from commit d7c0cd05a4ea9cd55f34812170ae7d3ca7f12fe5)

--- includes/GlobalUserGroupMembership.php
+++ includes/GlobalUserGroupMembership.php
@@ -322,9 +322,9 @@ class GlobalUserGroupMembership extends UserGroupMembership {
 		}
 
 		if ( $userName !== null ) {
-			$groupName = self::getGroupMemberName( $group, $userName );
+			$groupName = $context->getLanguage()->getGroupMemberName( $group, $userName );
 		} else {
-			$groupName = self::getGroupName( $group );
+			$groupName = $context->getLanguage()->getGroupName( $group );
 		}
 
 		// link to the group description page, if it exists
