abe8d5aab8d83e9ac20091d11fa6a62e28d0970f
Marcin Szwarc <marcinszwarc@hotmail.com>
2025-02-02 19:13:16 +0100

    Clearly display changes to the global user groups
    Adapts the GlobalRightsLogFormatter to refactorings in core's
    RightsLogFormatter and displays formatted changes to the user
    groups, instead of the former from / to.
    
    Bug: T369466
    Depends-On: I80d22ef7cbdc60a1437e26b6f9a6ee11bbecab64
    Change-Id: Ic44e86bc25ef0c917cd038b8b26db72ca584b20a

--- includes/GlobalRightsLogFormatter.php
+++ includes/GlobalRightsLogFormatter.php
@@ -22,69 +22,14 @@ class GlobalRightsLogFormatter extends RightsLogFormatter {
 		$key = parent::getMessageKey();
 
 		$params = $this->getMessageParameters();
-		if ( !isset( $params[4] ) ) {
+		if ( !$this->shouldProcessParams( $params ) ) {
 			$key = 'gur-rightslog-entry';
 		}
 
 		return $key;
 	}
 
-	protected function getMessageParameters() {
-		// This is hacky but required, because the parent RightsLogFormatter's method
-		// must be avoided, otherwise the group expiration date appears twice in the logs
-		$params = LogFormatter::getMessageParameters();
-
-		// Old entries do not contain a fourth parameter
-		if ( !isset( $params[4] ) ) {
-			return $params;
-		}
-
-		$oldGroups = $this->makeGroupArray( $params[3] );
-		$newGroups = $this->makeGroupArray( $params[4] );
-
-		$userName = $this->entry->getTarget()->getText();
-		if ( !$this->plaintext && count( $oldGroups ) ) {
-			foreach ( $oldGroups as &$group ) {
-				$group = $this->context->getLanguage()->getGroupMemberName( $group, $userName );
-			}
-		}
-		if ( !$this->plaintext && count( $newGroups ) ) {
-			foreach ( $newGroups as &$group ) {
-				$group = $this->context->getLanguage()->getGroupMemberName( $group, $userName );
-			}
-		}
-
-		// fetch the metadata about each group membership
-		$allParams = $this->entry->getParameters();
-
-		if ( count( $oldGroups ) ) {
-			$params[3] = Message::rawParam( $this->formatRightsList( $oldGroups,
-				isset( $allParams['oldmetadata'] ) ? $allParams['oldmetadata'] : [] ) );
-		} else {
-			$params[3] = $this->msg( 'rightsnone' )->text();
-		}
-
-		if ( count( $newGroups ) ) {
-			// Array_values is used here because of T44211
-			// see use of array_unique in UserrightsPage::doSaveUserGroups on $newGroups.
-			$params[4] = Message::rawParam( $this->formatRightsList( array_values( $newGroups ),
-				isset( $allParams['newmetadata'] ) ? $allParams['newmetadata'] : [] ) );
-		} else {
-			$params[4] = $this->msg( 'rightsnone' )->text();
-		}
-
-		$params[5] = $userName;
-
-		return $params;
-	}
-
-	private function makeGroupArray( $group ) {
-		// Migrate old group params from string to array
-		if ( $group === '' ) {
-			$group = [];
-		} elseif ( is_string( $group ) ) {
-			$group = array_map( 'trim', explode( ',', $group ) );
-		}
-		return $group;
+	protected function shouldProcessParams( $params ) {
+		return isset( $params[4] );
 	}
 }
